88b0c54da1050b70d70e406baa3d3ff87475f8ae
Josh Hobson <joshuahobson1995@gmail.com>
2022-12-10 10:28:35 -0700

    rollback

--- server/util/database.js
+++ server/util/database.js
@@ -1,5 +1,4 @@
 require('dotenv').config()
-require('pg')
 const {CONNECTION_STRING} = process.env
 const Sequelize = require('sequelize')
 
@@ -7,8 +6,6 @@ const Sequelize = require('sequelize')
 
 const sequelize = new Sequelize(CONNECTION_STRING, {
     dialect: 'postgres', 
-    protocol: 'postgres',
-    logging: true,
     dialectOptions: {
         ssl: {
             require: true,
@@ -17,21 +14,11 @@ const sequelize = new Sequelize(CONNECTION_STRING, {
     }
 })
 
-const sequelizeConnection = async () => {
-    try {
-        sequelize
-        .authenticate()
-        .then(() => {
-            console.log("Postgres connection has been established")
-        })
-    } catch (error){
-        console.log("Unable to connect to databaze:", error)
-    }
-}
+
 
 module.exports = {
-    sequelize,
-    sequelizeConnection
+    sequelize
+   
 
 }
 
